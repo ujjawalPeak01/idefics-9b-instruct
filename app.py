2d898fcd7e3b6134fd499384ade0dc6d06a7055c
ujjawalPeak01 <119571874+ujjawalPeak01@users.noreply.github.com>
2023-10-13 14:16:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
 import json
 import numpy as np
 import torch
-from transformers import pipeline
+from transformers import IdeficsForVisionText2Text, AutoProcessor
 
 
 class InferlessPythonModel:
 
     # Implement the Load function here for the model
     def initialize(self):
-        self.generator = pipeline("text-generation", model="HuggingFaceM4/idefics-9b-instruct", device=0)
+        checkpoint = "HuggingFaceM4/idefics-9b"
+        model = IdeficsForVisionText2Text.from_pretrained(checkpoint, torch_dtype=torch.bfloat16).to("cuda")
+        processor = AutoProcessor.from_pretrained(checkpoint)
 
     
     # Function to perform inference 
     def infer(self, inputs):
-        prompt = inputs["prompt"]
-        pipeline_output = self.generator(prompt, do_sample=True, max_new_tokens=200)
-        generated_txt = pipeline_output[0]["generated_text"]
-        print("Text: ", generated_txt)
+        prompts = [
+                "https://upload.wikimedia.org/wikipedia/commons/8/86/Id%C3%A9fix.JPG",
+                "In this picture from Asterix and Obelix, we can see"
+            ],
+        ]
+        inputs = processor(prompts, return_tensors="pt").to("cuda")
+        bad_words_ids = processor.tokenizer(["<image>", "<fake_token_around_image>"], add_special_tokens=False).input_ids
 
-        return {"generated_text": generated_txt}
+        generated_ids = model.generate(**inputs, bad_words_ids=bad_words_ids, max_length=100)
+        generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)
+        final_result = ""
+        for i, t in enumerate(generated_text):
+            print(f"{i}:\n{t}\n")
+            final_result += f"{i}:\n{t}\n"
+
+        return {"generated_text": final_result}
 
     # perform any cleanup activity here
     def finalize(self,args):
-        self.pipe = None
+        self.model = None
+        self.processor = None
+        
